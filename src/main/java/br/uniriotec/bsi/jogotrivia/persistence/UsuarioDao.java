7eed83df8d8b14e01bcc473f98bf066fd42f6757
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-09-14 17:46:57 -0300

    Create UsuarioDao.java

---
+++ src/main/java/br/uniriotec/bsi/jogotrivia/persistence/UsuarioDao.java
@@ -0,0 +1,10 @@
+package br.uniriotec.bsi.jogotrivia.persistence;
+
+import br.uniriotec.bsi.jogotrivia.administrativo.Usuario;
+
+public class UsuarioDao extends Dao<Usuario>{
+
+	public UsuarioDao() {
+		super(Usuario.class);
+	}
+}
