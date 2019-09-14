9e1faa8f1f3cd9f2a5b484851b0c09987b5ada7b
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-09-14 17:46:23 -0300

    Anotações JPA em Usuario

--- src/main/java/br/uniriotec/bsi/jogotrivia/administrativo/Usuario.java
+++ src/main/java/br/uniriotec/bsi/jogotrivia/administrativo/Usuario.java
@@ -2,17 +2,32 @@ package br.uniriotec.bsi.jogotrivia.administrativo;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * 
  * @author Mateus Bandeira
  * Criado em 13/09/19
  *
  */
+@Entity
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(nullable = false)
 	private String nome;
+	@Column(nullable = false)
 	private String hashSenha;
+	@Column(unique=true, nullable = false)
 	private String email;
+	@Column(nullable = false, columnDefinition = "datetime default current_timestamp")
 	private Date dataCadastro;
+	@Column(nullable = false, columnDefinition = "bit default 1")
 	private Boolean ativo;
 	
 	public Usuario(String nome, String hashSenha, String email, Date dataCadastro, Boolean ativo) {
