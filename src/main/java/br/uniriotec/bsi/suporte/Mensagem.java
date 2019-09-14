d263ec88579a8a761ebf7b92e81eb1d61559ea09
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-09-13 23:29:37 -0300

    Atualiza relacionamentos

--- src/main/java/br/uniriotec/bsi/suporte/Mensagem.java
+++ src/main/java/br/uniriotec/bsi/suporte/Mensagem.java
@@ -2,7 +2,10 @@ package br.uniriotec.bsi.suporte;
 
 import java.util.Date;
 
+import br.uniriotec.bsi.administrativo.Usuario;
+
 public class Mensagem {
 	private String conteudo;
 	private Date dataCriacao;
+	private Usuario autor;
 }
