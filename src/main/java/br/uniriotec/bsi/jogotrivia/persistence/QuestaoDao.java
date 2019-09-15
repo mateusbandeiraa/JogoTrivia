edfbc6d7253fe9593f24e89e64d36dc401378779
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-09-15 18:00:44 -0300

    Create QuestaoDao.java

---
+++ src/main/java/br/uniriotec/bsi/jogotrivia/persistence/QuestaoDao.java
@@ -0,0 +1,11 @@
+package br.uniriotec.bsi.jogotrivia.persistence;
+
+import br.uniriotec.bsi.jogotrivia.gameplay.Questao;
+
+public class QuestaoDao extends Dao<Questao>{
+
+	public QuestaoDao() {
+		super(Questao.class);
+	}
+
+}
