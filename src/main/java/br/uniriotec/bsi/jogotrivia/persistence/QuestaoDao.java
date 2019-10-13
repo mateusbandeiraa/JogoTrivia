b5a4e5669d875560560e2cf62f95d0f1b8a6c428
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-10-13 17:23:24 -0300

    Autor questão

--- src/main/java/br/uniriotec/bsi/jogotrivia/persistence/QuestaoDao.java
+++ src/main/java/br/uniriotec/bsi/jogotrivia/persistence/QuestaoDao.java
@@ -1,11 +1,28 @@
 package br.uniriotec.bsi.jogotrivia.persistence;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import br.uniriotec.bsi.jogotrivia.gameplay.Questao;
 
-public class QuestaoDao extends Dao<Questao>{
+public class QuestaoDao extends Dao<Questao> {
 
 	public QuestaoDao() {
 		super(Questao.class);
+	}
+
+	public List<Questao> selectByAutor(Integer id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Query q = em.createQuery("FROM " + Questao.class.getSimpleName() + " WHERE autor_id = ?1");
+		q.setParameter(1, id);
+		@SuppressWarnings("unchecked")
+		List<Questao> result = q.getResultList();
+		em.getTransaction().commit();
+		em.close();
+		return result;
 	}
 
 }
