e5990c443744cee3acfda6da96f358fb5b521943
mateusbandeiraa <mateusbandeiraa@gmail.com>
2020-04-10 23:03:57 -0300

    Create ViewUsuario.java

---
+++ src/main/java/br/uniriotec/bsi/jogotrivia/view/ViewUsuario.java
@@ -0,0 +1,41 @@
+package br.uniriotec.bsi.jogotrivia.view;
+
+public class ViewUsuario {
+	/**
+	 * Campos que o proprio usuario pode acessar
+	 */
+	public static class Proprio {
+		public static class Parametros {
+			/**
+			 * Campos de entrada no endpoint de cadastro
+			 */
+			public static class Cadastrar {
+			}
+
+			/**
+			 * Campos de entrada no endpoint de atualizacao
+			 */
+			public static class Atualizar {
+			}
+		}
+	}
+
+	/**
+	 * Campos que moderadores podem acessar
+	 */
+	public static class Moderador extends Proprio {
+		public static class Parametros {
+			/**
+			 * Campos de entrada no endpoint de cadastro
+			 */
+			public static class Cadastrar extends Proprio.Parametros.Cadastrar {
+			}
+
+			/**
+			 * Campos de entrada no endpoint de atualizacao
+			 */
+			public static class Atualizar extends Proprio.Parametros.Atualizar {
+			}
+		}
+	}
+}
