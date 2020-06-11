96c04dfcd800fab7f71da50c9f8c8a8cb078c4e1
mateusbandeiraa <mateusbandeiraa@gmail.com>
2020-06-11 19:04:29 -0300

    Rename formDataToJSON

--- WebContent/js/common-script.js
+++ WebContent/js/common-script.js
@@ -1,6 +1,6 @@
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-function FormDataToJSON(formData) {
+function formDataToJSON(formData) {
     var object = {};
     formData.forEach((value, key) => {
         // Reflect.has in favor of: object.hasOwnProperty(key)
