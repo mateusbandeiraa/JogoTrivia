75997d7daa4ab74d0e357b09c4b228f812fd22d2
mateusbandeiraa <mateusbandeiraa@gmail.com>
2020-06-11 18:33:17 -0300

    Add sign in functionality

---
+++ WebContent/js/common-script.js
@@ -0,0 +1,17 @@
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function FormDataToJSON(formData) {
+    var object = {};
+    formData.forEach((value, key) => {
+        // Reflect.has in favor of: object.hasOwnProperty(key)
+        if (!Reflect.has(object, key)) {
+            object[key] = value;
+            return;
+        }
+        if (!Array.isArray(object[key])) {
+            object[key] = [object[key]];
+        }
+        object[key].push(value);
+    });
+    return JSON.stringify(object);
+}
